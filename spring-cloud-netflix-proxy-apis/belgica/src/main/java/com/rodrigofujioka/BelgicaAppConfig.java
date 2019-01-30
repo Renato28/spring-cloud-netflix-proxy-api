fc526ee0a5e74fe010bb41effec9fa02ed44001d
Renato Nóbrega <renatonobrega87@gmail.com>
2019-01-30 18:15:06 -0300

    projeto atualizado

---
+++ spring-cloud-netflix-proxy-apis/belgica/src/main/java/com/rodrigofujioka/BelgicaAppConfig.java
@@ -0,0 +1,16 @@
+package com.rodrigofujioka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BelgicaAppConfig {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BelgicaAppConfig.class, args);
+	}
+
+}
